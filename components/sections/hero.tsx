fd3d80bac64f3eecff56b2b37e0721ad4493f23f
Ahmad <ahmadaquino.2002@gmail.com>
2026-02-18 15:27:06 +0800

    Restart project

---
+++ components/sections/hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <section className="py-20 text-center">
+      <h1 className="text-5xl font-bold">
+        My Landing Page 🚀
+      </h1>
+    </section>
+  )
+}
